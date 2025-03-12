7b3b8b2539aa3dde16ee1d5b53b4072f81a40a7f
woshikedayaa <woshikedayaa@gmail.com>
2025-03-12 16:53:38 +0800

    refactor(ui): replace systray to qt because https://github.com/fyne-io/systray/issues/72

--- cmd/boxtray/boxtray.go
+++ cmd/boxtray/boxtray.go
@@ -2,7 +2,7 @@ package boxtray
 
 import (
 	"fmt"
-	"fyne.io/systray"
+	qt "github.com/mappu/miqt/qt6"
 	"github.com/woshikedayaa/boxtray/common/capi"
 	"github.com/woshikedayaa/boxtray/config"
 	"github.com/woshikedayaa/boxtray/log"
@@ -27,6 +27,51 @@ func Main(cfg config.Config) {
 		logger.Error("Create Client", slog.String("error", err.Error()))
 		return
 	}
-	global = InitGlobal(cfg, client)
-	systray.Run(onReady, onExit)
+	_ = qt.NewQApplication(os.Args)
+
+	tray1 := qt.NewQSystemTrayIcon2(qt.QApplication_Style().StandardIcon(qt.QStyle__SP_ComputerIcon, nil, nil))
+	tray1.OnActivated(func(reason qt.QSystemTrayIcon__ActivationReason) {
+		if reason != qt.QSystemTrayIcon__Trigger {
+			return
+		}
+		fmt.Println("Main Click")
+	})
+	menu := qt.NewQMenu2()
+	actionOpen := qt.NewQAction2("Click me")
+	actionOpen.SetCheckable(true)
+	actionOpen.OnTriggered(func() {
+		actionOpen.SetChecked(false)
+		fmt.Println("Open Clicked")
+	})
+
+	subA := qt.NewQAction2("Sub Action")
+	subA.SetCheckable(true)
+	subA.OnTriggered(func() {
+		fmt.Println("sub check")
+		// subA.SetChecked(!subA.IsChecked())
+		subA.SetChecked(false)
+	})
+	subMenu := qt.NewQMenu3("Sub Menu")
+	subMenu.AddAction(subA)
+	menu.AddAction(actionOpen)
+	menu.AddMenu(subMenu)
+	actionGroup := qt.NewQActionGroup(tray1.QObject)
+	actionGroup.SetExclusive(true)
+	btn1 := qt.NewQAction2("a1")
+	btn1.SetCheckable(true)
+	btn2 := qt.NewQAction2("a2")
+	btn2.SetCheckable(true)
+	btn3 := qt.NewQAction2("a3")
+	btn3.SetCheckable(true)
+	actionGroup.AddAction(btn1)
+	actionGroup.AddAction(btn2)
+	actionGroup.AddAction(btn3)
+
+	menu.AddAction(btn1)
+	menu.AddAction(btn2)
+	menu.AddAction(btn3)
+
+	tray1.SetContextMenu(menu)
+	tray1.Show()
+	qt.QApplication_Exec()
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/woshikedayaa/boxtray
 go 1.24.0
 
 require (
-	fyne.io/systray v1.11.0 // indirect
-	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	golang.org/x/sys v0.31.0 // indirect
+	github.com/gorilla/websocket v1.5.3
+	github.com/mappu/miqt v0.9.0 // indirec
 )
